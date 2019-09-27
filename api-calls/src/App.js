634837c8fe1456e456c275fdc0c58096dd00a4fd
loferris <lo.k.ferris@gmail.com>
2019-09-27 14:09:48 -0700

    broken prototypes

---
+++ api-calls/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import styled from 'styled-components';
+//import bootstrapStyled from 'bootstrap-styled';
+
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+function App() {
+  return (
+  <Wrapper>
+    <div className="App">
+    </div>
+  </Wrapper>
+  );
+}
+
+export default App;
