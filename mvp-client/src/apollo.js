ca5e58bf2b528620e9ae87df804328bdcbf3204d
loferris <lo.k.ferris@gmail.com>
2019-10-30 13:15:54 -0700

    file upload with apollo-client and apollo-server set up before cloud migration

---
+++ mvp-client/src/apollo.js
@@ -0,0 +1,11 @@
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
+
+const link = createUploadLink({ url: 'http://localhost:4000/graphql' });
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
--- mvp-client/src/index.js
+++ mvp-client/src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from './apollo';
 
-render(<ApolloProvider><App /></ApolloProvider>, document.getElementById('root'));
+render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
