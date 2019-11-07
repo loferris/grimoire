36ce0bee3a8d9cf747fe0b8cd2774dd675a9d573
loferris <lo.k.ferris@gmail.com>
2019-11-07 11:52:16 -0800

    card editor set up

--- firebase-client/src/views/UserUploads/index.js
+++ firebase-client/src/views/UserUploads/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FileUpload from "./FileUpload";
+import CardEditor from "./CardEditor";
 
 const UploadPage = () => {
   return (
     <div>
       <h2>upload an image to create your own oracle card</h2>
       <FileUpload />
+      <CardEditor />
     </div>
   );
 };
