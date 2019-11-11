c0eff224e9d63a0164197510c6321d04e74fc34a
loferris <lo.k.ferris@gmail.com>
2019-11-11 11:34:33 -0800

    more apollo boilerplate

--- mvp-server/index.js
+++ mvp-server/index.js
@@ -1,4 +1,14 @@
-const { ApolloServer, gql } = require("apollo-server");
+require('dotenv').config();
 
-const typeDefs = gql`
-`;
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const port = process.env.PORT
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 || port }, () => console.log(`Apollo Server ready at http://localhost:${port}${server.graphqlPath}`) ); 
