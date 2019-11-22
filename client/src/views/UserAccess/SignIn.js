9a2cc3b4ebbfcc4a2cc689ce9c74b731ef9aa9e2
loferris <lo.k.ferris@gmail.com>
2019-11-22 12:10:53 -0800

    refactored sign in

--- client/src/views/UserAccess/SignIn.js
+++ client/src/views/UserAccess/SignIn.js
@@ -1,23 +1,16 @@
-import React, { Component } from "react";
-//import styled from "@emotion/styled";
-//import { rhythm } from "../../utils/typography";
+import React, { useState, useEffect } from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase/app";
 import "firebase/auth";
 import { client } from "../../utils/apollo";
 import { USER_MUTATION } from "../../components/Mutation/UserSync";
 
-class SignIn extends Component {
-  state = {
-    isSignedIn: false, // Local signed-in state.
-    uid: "test"
-  };
-
+const SignIn = () => {
   // Configure FirebaseUI.
-  uiConfig = {
+  const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: "popup",
-    // We will display Google and Facebook as auth providers.
+    // We will display Google as auth provider.
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID
       //firebase.auth.FacebookAuthProvider.PROVIDER_ID
@@ -29,48 +22,47 @@ class SignIn extends Component {
     }
   };
 
+  const [isSignedIn, setSignIn] = useState(false);
+
   // Listen to the Firebase Auth state and set the local state.
-  componentDidMount() {
-    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user });
-      this.setState({ uid: user.uid });
-      console.log(this.state.uid); //test
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setSignIn(!!user);
+      console.log(user.uid); //test
       client.mutate({
         mutation: USER_MUTATION,
-        variables: { objects: [{ fire_uid: this.state.uid }] }
+        variables: { objects: [{ fire_uid: user.uid }] }
       });
-      //this component is currently calling the unique firebase uid from the user object created by the auth admin SDK, and setting it as a prop of the local state of this component. I have visually confirmed this uid is what I am looking for and what I want to send to my postgres database
+      //this component is currently calling the unique firebase uid from the user object created by the auth admin SDK
     });
-  }
+  }, [isSignedIn]);
 
   // Make sure we un-register Firebase observers when the component unmounts.
-  componentWillUnmount() {
+  /*componentWillUnmount() {
     this.unregisterAuthObserver();
-  }
+  }*/
 
-  render() {
-    if (!this.state.isSignedIn) {
-      return (
-        <div>
-          <h1>welcome</h1>
-          <p>please sign in to create your own book of shadows</p>
-          <StyledFirebaseAuth
-            uiConfig={this.uiConfig}
-            firebaseAuth={firebase.auth()}
-          />
-        </div>
-      );
-    }
+  if (!isSignedIn) {
     return (
       <div>
-        <p>
-          Welcome {firebase.auth().currentUser.displayName}! You are now
-          signed-in!
-        </p>
-        <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+        <h1>welcome</h1>
+        <p>please sign in to create your own book of shadows</p>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
       </div>
     );
   }
-}
+  return (
+    <div>
+      <p>
+        Welcome {firebase.auth().currentUser.displayName}! You are now
+        signed-in!
+      </p>
+      <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+    </div>
+  );
+};
 
 export default SignIn;
