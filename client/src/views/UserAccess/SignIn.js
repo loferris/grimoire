c62b2ccd708ad02233d763e60ff23b5bdda726f1
loferris <lo.k.ferris@gmail.com>
2019-11-22 13:59:42 -0800

    refactoring sign in

--- client/src/views/UserAccess/SignIn.js
+++ client/src/views/UserAccess/SignIn.js
@@ -5,25 +5,24 @@ import "firebase/auth";
 import { client } from "../../utils/apollo";
 import { USER_MUTATION } from "../../components/Mutation/UserSync";
 
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // We will display Google as auth provider.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    //firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    //firebase.auth.EmailAuthProvider.credential(email, password)
+  ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
 const SignIn = () => {
-  // Configure FirebaseUI.
-  const uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: "popup",
-    // We will display Google as auth provider.
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID
-      //firebase.auth.EmailAuthProvider.credential(email, password)
-    ],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false
-    }
-  };
-
   const [isSignedIn, setSignIn] = useState(false);
-
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
