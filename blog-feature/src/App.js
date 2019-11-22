c677e712cebe02b47c4cffaadee28a241aee7f4e
loferris <lo.k.ferris@gmail.com>
2019-11-22 10:11:05 -0800

    cleaned up repo

--- blog-feature/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-//import UserEditor from "./components/UserEditor";
-import QuillEditor from "./components/Quill";
-
-export default function App() {
-  return <QuillEditor />;
-}
